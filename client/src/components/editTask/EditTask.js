97b6f111023acdf9b497e362304ac8c66c8adb1c
LakmaSanjula-97 <lakmagamage97@gmail.com>
2023-11-11 15:44:24 +0530

    Edit task details

---
+++ client/src/components/editTask/EditTask.js
@@ -0,0 +1,140 @@
+import React, { Fragment, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function EditTask({ task }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
+  const [cretaed_at, setCretaed_at] = useState(task.cretaed_at);
+  const [status, setStatus] = useState(task.status);
+
+  const updteTask = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { title, description, cretaed_at, status };
+      const response = await fetch(
+        `http://localhost:5000/tasks/${task.task_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      console.log(response);
+      window.location = "/list";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <div>
+          <Button
+          onClick={handleOpen}
+          size="small"
+          variant="contained"
+          color="primary"
+        >
+          EDIT
+        </Button>
+        </div>
+        
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h4" component="h2">
+              EDIT TASK
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Title"
+                    fullWidth
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Description"
+                    fullWidth
+                    required
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="date"
+                    fullWidth
+                    required
+                    value={cretaed_at}
+                    onChange={(e) => setCretaed_at(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <InputLabel id="status-label">Status</InputLabel>
+                    <Select
+                      labelId="status-label"
+                      id="status"
+                      label="Status"
+                      value={status}
+                      onChange={(e) => setStatus(e.target.value)}
+                    >
+                      <MenuItem value="To Do">To Do</MenuItem>
+                      <MenuItem value="In Progress">In Progress</MenuItem>
+                      <MenuItem value="Completed">Completed</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} justifyContent="center" display="flex">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="button"
+                    onClick={(e) => updteTask(e)}
+                  >
+                    EDIT
+                  </Button>
+                </Grid>
+              </Grid>
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+    </Fragment>
+  );
+}
