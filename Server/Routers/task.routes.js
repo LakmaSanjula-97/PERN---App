d49be754a600682f0347726ca4cfea2deb7a574c
LakmaSanjula-97 <lakmagamage97@gmail.com>
2023-11-11 15:46:32 +0530

    Task manae routers

---
+++ Server/Routers/task.routes.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const pool = require("../db");
+
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const { title, description, cretaed_at, status } = req.body;
+    const newTask = await pool.query(
+      "INSERT INTO task(title,description,cretaed_at,status) VALUES($1,$2,$3,$4) RETURNING *",
+      [title, description, cretaed_at, status]
+    );
+
+    res.json(newTask.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// VIEW
+
+router.get("/", async (req, res) => {
+  try {
+    const allTasks = await pool.query("SELECT * FROM task");
+    res.json(allTasks.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// GET A SINGLE TASK
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await pool.query("SELECT * FROM task WHERE task_id = $1", [
+      id,
+    ]);
+
+    res.json(task.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// UPDATE A TASK
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, cretaed_at, status } = req.body;
+
+    const updateTask = await pool.query(
+      "UPDATE task SET title=$1, description=$2, cretaed_at=$3, status=$4 WHERE task_id=$5",
+      [title, description, cretaed_at, status, id]
+    );
+
+    res.json("Task was updated");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// DELETE A TASK
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteTask = await pool.query("DELETE FROM task WHERE task_id=$1", [
+      id,
+    ]);
+
+    res.json("Task was deleted");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+module.exports = router;
